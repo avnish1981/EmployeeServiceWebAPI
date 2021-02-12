4b23fb414ffeb3db191f0ca49d4c519d487e4a87
avnish1981 <avnish.choubey@gmail.com>
2021-02-12 13:10:15 +0530

    checkin files

---
+++ EmployeeServiceWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmpDataAccess.Model;
+
+namespace EmployeeServiceWebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        [Authorize]
+        public HttpResponseMessage Employees()
+        {
+            try
+            {
+                using (EmpDBConnection entities = new EmpDBConnection())
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.ToList());
+                }
+
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message );
+            }
+            
+        }
+    }
+}
